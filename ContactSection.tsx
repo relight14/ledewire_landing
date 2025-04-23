0bc85fcaa44d0f5ce408d81e7a6a5bb137776f0b
relight14 <morgan@relightnews.com>
2025-04-23 11:46:23 -0600

    Update ContactSection.tsx

--- ContactSection.tsx
+++ ContactSection.tsx
@@ -38,7 +38,7 @@ const ContactSection: React.FC = () => {
                 </div>
                 <div>
                   <h3 className="text-lg font-medium text-gray-900">Office</h3>
-                  <p className="text-gray-600">San Francisco, CA</p>
+                  <p className="text-gray-600">Park City, UT</p>
                 </div>
               </div>
               
@@ -48,7 +48,7 @@ const ContactSection: React.FC = () => {
                 </div>
                 <div>
                   <h3 className="text-lg font-medium text-gray-900">Phone</h3>
-                  <p className="text-gray-600">(555) 123-4567</p>
+                  <p className="text-gray-600">(415) 324-9523</p>
                 </div>
               </div>
             </div>
