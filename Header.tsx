ae5cc8d4c16206225980028fa271463ccefc3bb5
relight14 <morgan@relightnews.com>
2025-04-30 14:44:50 -0600

    Update Header.tsx

--- Header.tsx
+++ Header.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import Logo from '../assets/LedeWireLogo.png';
+import Logo from '../assets/LedeWireLogo.svg';
 
 const Header: React.FC = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
